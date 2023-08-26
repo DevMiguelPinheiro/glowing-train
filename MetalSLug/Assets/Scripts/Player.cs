4d02be73483f69df131e1f2b62abbac95b05106d
DevMiguelPinheiro <leitemiguelpinheiro@gmail.com>
2023-08-25 22:15:49 -0400

    Movimentacao do Player e Animacoes
    adicionado a movimentacao e checagem do chao e morte, e animacoes de pular,correr,atirar e Idle

--- MetalSLug/Assets/Scripts/Player.cs
+++ MetalSLug/Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 600;
-    private Rigidbody2D rb2d = 5f;
+    private Rigidbody2D rb2d;
     private bool facingRight = true;
     private bool jump;
     private bool onGround = false;
@@ -14,12 +14,11 @@ public class Player : MonoBehaviour
     private float hForce = 0;
     private bool isDead = false;
 
-
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        groundCheck = gameObject.transform.Find("GroundCheck");
+        groundCheck = transform.Find("GroundCheck");
     }
 
     // Update is called once per frame
@@ -27,21 +26,49 @@ public class Player : MonoBehaviour
     {
         if (!isDead)
         {
-            onGround = Physics2D.Linecast(tranform.position,groundCheck.position,1<< LayerMask.NameToLayer("Ground"));
-            if(Input.GetMouseButton("Jump") && onGround) 
+            onGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+            if (Input.GetMouseButtonDown(0) && onGround)
             {
                 jump = true;
             }
-            else if (Input.GetMouseButton("Jump")) 
-            { 
-                if(rb2d.velocity.y > 0) 
-                { 
-                rb2d.velocity.y = 0; = newVector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
+            else if (Input.GetMouseButtonDown(0))
+            {
+                if (rb2d.velocity.y > 0)
+                {
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
                 }
             }
         }
+    }
 
+    private void FixedUpdate()
+    {
+        if (!isDead)
+        {
+            hForce = Input.GetAxisRaw("Horizontal");
+            rb2d.velocity = new Vector2(hForce * speed, rb2d.velocity.y);
+            if (hForce > 0 && !facingRight)
+            {
+                Flip();
+            }
+            else if (hForce < 0 && facingRight)
+            {
+                Flip();
+            }
+            if (jump)
+            {
+                jump = false;
+                rb2d.AddForce(Vector2.up * jumpForce);
+            }
+        }
+    }
 
+    void Flip()
+    {
+        facingRight = !facingRight;
 
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
